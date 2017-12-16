b85ff86f5933fcf3d5151a0449ad4415358fe014
kvikram1988 <kvikram1988@gmail.com>
2017-12-16 16:36:19 +0530

    Adding test case data to set implementation.

--- js/set.js
+++ js/set.js
@@ -38,7 +38,7 @@
 	}
 
 	MySet.prototype.intersection = function (otherSet) {
-		let intersectSet = otherSet.storage.map((otherSetItem) => {
+		let intersectSet = otherSet.storage.filter((otherSetItem) => {
 			if (this.has(otherSetItem)) {
 				return true;
 			}
@@ -55,4 +55,19 @@
 		});
 		return true;
 	}
+
+	let set1 = new MySet(),
+			set2 = new MySet();
+
+	console.log("/*--- Set implementation ---*/")
+	console.log("Push 1 into set1: " + set1.add(1));
+	console.log("Push 2 into set1: " + set1.add(2));
+	console.log("Push 1 into set2: " + set2.add(1));
+	console.log("Push 3 into set2: " + set2.add(3));
+	console.log("Push 5 into set2: " + set2.add(5));
+	console.log("Push 5 again into set2: " + set2.add(5));
+	console.log("Remove 5 from set2: " + set2.remove(5));
+	console.log("Union of set1 and set2: " + set1.union(set2));
+	console.log("Intersection of set1 and set2: " + set1.intersection(set2));
+	console.log("Is set2 subset of set1: " + set1.subset(set2));
 })();
