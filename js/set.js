37b2da1c93a90d8d4b563b20579bf9f8e3c043ab
kvikram1988 <kvikram1988@gmail.com>
2017-12-16 15:59:29 +0530

    Complete Set functionality.

--- js/set.js
+++ js/set.js
@@ -1,35 +1,58 @@
-let Set = function () {
-	this.storage = [];
-}
+(function () {
+	let MySet = function () {
+		this.storage = [];
+	}
 
-Set.prototype.values = function () {
-	return this.storage;
-}
-Set.storage.has = function (item) {
-	if(this.storage.indexOf(item) >= 0) {
+	MySet.prototype.values = function () {
+		return this.storage;
+	}
+	MySet.prototype.has = function (item) {
+		if (this.storage.indexOf(item) >= 0) {
+			return true;
+		}
+		return false;
+	}
+
+	MySet.prototype.add = function (item) {
+		if (!this.has(item)) {
+			this.storage.push(item);
+		}
+		return this.storage;
+	}
+
+	MySet.prototype.remove = function (item) {
+		if (this.has(item)) {
+			let index = this.storage.indexOf(item);
+			this.storage.splice(index, 1);
+		}
+		return this.storage;
+	}
+
+	MySet.prototype.union = function (otherSet) {
+		otherSet.storage.forEach((otherSetItem) => {
+			if (!this.has(otherSetItem)) {
+				this.add(otherSetItem);
+			}
+		})
+		return this.storage;
+	}
+
+	MySet.prototype.intersection = function (otherSet) {
+		let intersectSet = otherSet.storage.map((otherSetItem) => {
+			if (this.has(otherSetItem)) {
+				return true;
+			}
+			return false;
+		});
+		return intersectSet;
+	}
+
+	MySet.prototype.subset = function (otherSet) {
+		otherSet.storage.forEach((otherSetItem)=>{
+			if(!this.has(otherSetItem)){
+				return false;
+			}
+		});
 		return true;
 	}
-	return false;
-}
-
-Set.prototype.add = function (item) {
-	if(!this.has(item)) {
-		this.storage.push(item);
-	}
-	return this.storage;
-}
-
-Set.prototype.remove = function (item) {
-	if(this.has(item)) {
-		let index = this.storage.indexOf(item);
-		this.storage.splice(index, 1);
-	}
-	return this.storage;
-}
-
-Set.prototype.union = function (otherSet) {
-	otherSet.forEach((otherSetItem)=>{
-		this.storage.add(otherSetItem);
-	})
-	return this.storage;
-}
+})();
